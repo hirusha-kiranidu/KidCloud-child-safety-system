8c816462e872b307adf170b9db2d7feb71bdedc7
pasantharupathi <pasantharupathi1@gmail.com>
2026-03-03 12:46:13 +0530

    Initialize Firebase using service account credentials
    Added logic to initialize Firebase Admin SDK using a service
    account JSON file when available.

--- backend/core/firebase.py
+++ backend/core/firebase.py
@@ -4,3 +4,10 @@ from firebase_admin import credentials, firestore
 from .config import settings
 
 def initialize_firebase():
+
+    if not firebase_admin._apps:
+        try:
+            if os.path.exists(settings.FIREBASE_CREDENTIALS_PATH):
+                cred = credentials.Certificate(settings.FIREBASE_CREDENTIALS_PATH)
+                firebase_admin.initialize_app(cred)
+                print("Firebase Admin SDK initialized successfully.")
