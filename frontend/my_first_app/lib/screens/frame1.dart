d197e67eace344e53a8aae787e632b446e66ef43
Wathmi Kodippili <wathmipadmarasi@gmail.com>
2026-03-08 17:37:15 +0530

    update the frame 1(splash screen) text part

--- frontend/my_first_app/lib/screens/frame1.dart
+++ frontend/my_first_app/lib/screens/frame1.dart
@@ -5,8 +5,53 @@ class Frame1 extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(child: Text("Frame 1")),
+    return Scaffold(
+      body: Container(
+        width: double.infinity,
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              Color(0xFF9EC3D4),
+              Color(0xFFBFDAD2),
+              Color(0xFF9ED6B3),
+            ],
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+          ),
+        ),
+
+        child: Column(
+          children: [
+
+            const SizedBox(height: 120),
+
+            // App Name
+            const Text(
+              "KIDCLOUD",
+              style: TextStyle(
+                fontSize: 38,
+                fontWeight: FontWeight.bold,
+                letterSpacing: 6,
+                color: Color(0xFFFF8C2B),
+              ),
+            ),
+
+            const SizedBox(height: 10),
+
+            // Tagline
+            const Text(
+              "CARRY THEIR SAFETY, EVERYWHERE",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 14,
+                letterSpacing: 4,
+                color: Color(0xFF3A5A7A),
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
