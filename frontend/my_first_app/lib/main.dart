cd99253dd5db7ff3fda31258ce7899092970535b
binadi-28 <binadi.20240153@iit.ac.lk>
2026-03-10 15:20:28 +0530

    Connect SignupScreen with main.dart

--- frontend/my_first_app/lib/main.dart
+++ frontend/my_first_app/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'screens/signup_screen.dart';
 
 void main() {
   runApp(const KidCloudApp());
@@ -10,7 +11,8 @@ class KidCloudApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Scaffold(body: Center(child: Text("KidCloud"))),
+      debugShowCheckedModeBanner: false,
+      home: SignupScreen(),
     );
   }
 }
