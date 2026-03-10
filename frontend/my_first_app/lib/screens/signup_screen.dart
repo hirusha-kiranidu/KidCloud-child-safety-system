400604f349cb8f01eb4a62c89a0e773b76616de5
binadi-28 <binadi.20240153@iit.ac.lk>
2026-03-10 15:57:10 +0530

    Add gradient background to signup screen

--- frontend/my_first_app/lib/screens/signup_screen.dart
+++ frontend/my_first_app/lib/screens/signup_screen.dart
@@ -5,6 +5,18 @@ class SignupScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(body: Center(child: Text("Signup Screen")));
+    return Scaffold(
+      body: Container(
+        width: double.infinity,
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [Color(0xFFB3E5FC), Color(0xFFC8E6C9)],
+          ),
+        ),
+        child: const Center(child: Text("Signup Screen")),
+      ),
+    );
   }
 }
