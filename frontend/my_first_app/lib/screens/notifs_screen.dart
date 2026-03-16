b9524342cb0256f2a9fe1c33f961bbcb99c3f254
Sayuri Senara <sayuri.20232596@iit.ac.lk>
2026-03-16 11:17:30 +0530

    Create basic structure for NotifsScreen

---
+++ frontend/my_first_app/lib/screens/notifs_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class NotifsScreen extends StatefulWidget {
+  const NotifsScreen({super.key});
+
+  @override
+  State<NotifsScreen> createState() => _NotifsScreenState();
+}
+
+class _NotifsScreenState extends State<NotifsScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
