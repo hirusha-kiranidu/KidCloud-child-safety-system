655ac0cd3c1e937e478c7c812421451b59bf2e8e
Wathmi Kodippili <wathmipadmarasi@gmail.com>
2026-03-16 12:56:59 +0530

    Import shared widgets for reusable UI components

--- frontend/my_first_app/lib/screens/onboard_screen.dart
+++ frontend/my_first_app/lib/screens/onboard_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import '../theme/app_theme.dart';
+import '../widgets/shared_widgets.dart';
 
 class OnboardScreen extends StatelessWidget {
   final int idx;
