40a524c778fecd956607945718a5dcf0808219f5
Rashmika Sewmini <rashmikasewmini12@gmail.com>
2026-03-16 14:53:11 +0530

    Feature extraction and dataset preparation

---
+++ backend/app/services/AI/train_sound_model.py
@@ -0,0 +1,43 @@
+import pandas as pd
+import librosa
+import numpy as np
+import os
+from tqdm import tqdm
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+import pickle
+
+# Load metadata
+metadata = pd.read_csv("dataset/UrbanSound8K/metadata/UrbanSound8K.csv")
+
+features = []
+labels = []
+
+def extract_features(file_path):
+    audio, sample_rate = librosa.load(file_path, res_type='kaiser_fast')
+    mfcc = librosa.feature.mfcc(y=audio, sr=sample_rate, n_mfcc=40)
+    mfcc_scaled = np.mean(mfcc.T, axis=0)
+    return mfcc_scaled
+
+print("Extracting features from dataset...")
+
+for index, row in tqdm(metadata.iterrows(), total=len(metadata)):
+    
+    file_path = os.path.join(
+        "dataset/UrbanSound8K/audio/fold" + str(row["fold"]),
+        row["slice_file_name"]
+    )
+
+    data = extract_features(file_path)
+
+    features.append(data)
+    labels.append(row["class"])
+
+X = np.array(features)
+y = np.array(labels)
+
+print("Splitting dataset...")
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42
+)
