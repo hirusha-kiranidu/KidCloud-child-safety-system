b8a51ee46a439befa4a5c97b9cf23030a73eb57c
Wathmi Kodippili <wathmipadmarasi@gmail.com>
2026-03-22 15:13:57 +0530

    delete files

--- backend/core/config.py
+++
@@ -1,8 +0,0 @@
-import os
-from pydantic_settings import BaseSettings
-
-class Settings(BaseSettings):
-    PROJECT_NAME: str = "KidCloud API"
-    API_V1_STR: str = "/api/v1"
-
-    FIREBASE_CREDENTIALS_PATH: str = os.getenv("FIREBASE_CREDENTIALS_PATH", "firebase-service-account.json")
