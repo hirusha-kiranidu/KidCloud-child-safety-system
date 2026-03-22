d792ffb7022fc22a69960fdcae178e7514b543e4
binadi-28 <binadi.20240153@iit.ac.lk>
2026-03-22 14:52:39 +0530

    update utils folder

--- frontend/my_first_app/lib/utils/api_config.dart
+++ frontend/my_first_app/lib/utils/api_config.dart
@@ -6,7 +6,7 @@ const String kEndpointLogout = '/auth/logout';
 const String kEndpointMe = '/auth/me';
 const String kEndpointChildren = '/children';
 const String kEndpointAlerts = '/alerts';
-const String kEndpointLocation = '/location'; //
+const String kEndpointLocation = '/location';
 
 const String kTokenKey = 'kidcloud_access_token';
 const String kUserKey = 'kidcloud_user';
